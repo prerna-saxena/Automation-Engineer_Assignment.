38e81d7881c49754791733237849dfd7ed044c7a
PRERNA SAXENA <70446643+prerna-saxena@users.noreply.github.com>
2024-06-13 21:52:04 +0530

    b.bash

---
+++ b.bash
@@ -0,0 +1,6 @@
+pip install selenium
+pip install Appium-Python-Client
+pip install pillow
+pip install pytesseract
+sudo apt-get install tesseract-ocr  # For Linux
+brew install tesseract  # For macOS
